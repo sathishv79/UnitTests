1a620081d43ebaefd5853a962217d89e74e901bc
Sathish Velayudam <sathishv@gmail.com>
2016-04-24 12:25:32 -0400

    Deleted target files

---
+++ src/main/java/com/networked/unittests/ClassUnderTest.java
@@ -0,0 +1,16 @@
+package com.networked.unittests;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
+/**
+ * Created by sathishv on 4/22/16.
+ */
+public class ClassUnderTest {
+    private static final Logger LOG = LogManager.getLogger(ClassUnderTest.class.getCanonicalName());
+
+    public void methodThatLogs() {
+
+    }
+}
